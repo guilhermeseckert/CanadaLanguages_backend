3ba2a3374cfd814657bfb2d74699931036f984da
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-09-01 22:53:34 -0700

    add authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import {Router} from "express"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import  {CreateUserController } from "./controllers/CreateUserController"
+import {ensureAdmin} from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
-
+router.post("/login",authenticateUserController.handle);
 router.post("/users", createUserController.handle)
 
 
